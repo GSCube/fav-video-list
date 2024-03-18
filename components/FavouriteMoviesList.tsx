a4625597629bf709678cdf187c3d522bb36843d5
Grzegorz Stosio <0mcabra@gmail.com>
2024-03-18 20:55:17 +0100

    Add editor config

--- components/FavouriteMoviesList.tsx
+++ components/FavouriteMoviesList.tsx
@@ -1,160 +1,145 @@
 "use client"
-import { useSession } from 'next-auth/react'
-import { redirect } from 'next/navigation'
+import {useSession} from 'next-auth/react'
+import {redirect} from 'next/navigation'
 import React, {useEffect, useState} from "react";
-import { Typography, Button } from '@mui/material';
+import {Typography, Button} from '@mui/material';
 import {Video, VideoTable} from './Table';
 import {Modal} from "@mui/material";
 import {Box} from "@mui/system";
 import YouTube from "react-youtube";
 
 const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    boxShadow: 24,
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  boxShadow: 24,
 };
 
 const deleteVideo = async (accessToken: string, id: string) => {
-    if (!accessToken) {
-        return
-    }
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?id=${id}`, {
-        // const response = await fetch("https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true", {
-        method: 'DELETE',
+  if (!accessToken) {
+    return
+  }
+  const response = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?id=${id}`, {
+    // const response = await fetch("https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true", {
+    method: 'DELETE',
 
-        headers: {
-            // @ts-ignore
-            Authorization: `Bearer ${accessToken}`,
-            // Accept: "application/json",
-        },
-    });
+    headers: {
+      // @ts-ignore
+      Authorization: `Bearer ${accessToken}`,
+      // Accept: "application/json",
+    },
+  });
 
 
-    return response; // This will contain information about the user's favorite movies or videos
+  return response; // This will contain information about the user's favorite movies or videos
 };
 
 
 const fetchFavorites = async (accessToken: string) => {
-    if (!accessToken) {
-        return
-    }
-    const response = await fetch("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=LL", {
-        // const response = await fetch("https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true", {
-        headers: {
-            // @ts-ignore
-            Authorization: `Bearer ${accessToken ||tempToken}`,
-            Accept: "application/json",
-        },
-    });
+  if (!accessToken) {
+    return
+  }
+  const response = await fetch("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=LL", {
+    // const response = await fetch("https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true", {
+    headers: {
+      // @ts-ignore
+      Authorization: `Bearer ${accessToken || tempToken}`,
+      Accept: "application/json",
+    },
+  });
 
-    const data = await response.json();
-    console.log(data)
-    return data.items; // This will contain information about the user's favorite movies or videos
+  const data = await response.json();
+  console.log(data)
+  return data.items; // This will contain information about the user's favorite movies or videos
 };
 
 export const FavouriteMoviesList = () => {
-    const [open, setOpen] = React.useState(false);
-    const [selectedVideoId, setSelectedVideoId] = React.useState('');
-    const [favorites, setFavorites] = useState([]);
-    const { data: session } = useSession({
-        required: true,
-        onUnauthenticated() {
-            redirect('/api/auth/signin?callbackUrl=/')
-        }
+  const [open, setOpen] = React.useState(false);
+  const [selectedVideoId, setSelectedVideoId] = React.useState('');
+  const [favorites, setFavorites] = useState([]);
+  const {data: session} = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect('/api/auth/signin?callbackUrl=/')
+    }
+  })
+
+  const handleClose = () => {
+    console.log('close')
+    setOpen(false);
+  }
+  // @ts-ignore
+  console.log('session in component', session)
+  // console.log('session', session?.token?.token?.account?.access_token)
+  // @ts-ignore
+  const {accessToken} = session || {}
+
+
+  const handleFetchFavorites = () => {
+    fetchFavorites(accessToken).then((fav) => {
+      console.log('fav', fav)
+      setFavorites(fav)
     })
+  }
 
-    const handleClose = () =>{
-        console.log('close')
-        setOpen(false);
-    }
+  console.log('favorites', favorites)
+
+  useEffect(() => {
+    handleFetchFavorites()
+  }, [accessToken]);
+
+  const handleVideoDelete = async (id: string) => {
+    await deleteVideo(accessToken, id)
+    await fetchFavorites(accessToken).then((fav) => {
+      console.log('fav', fav)
+      setFavorites(fav)
+    })
+  }
+
+  const handlePlay = (id: string) => {
+    setSelectedVideoId(id)
+    setOpen(true)
+  }
+
+  console.log('favorites', favorites)
+
+  const ytVideos: Video[] = favorites?.map((favorite) => ({
     // @ts-ignore
-    console.log('session in component', session)
-    // console.log('session', session?.token?.token?.account?.access_token)
+    playlistElementId: favorite.id,
     // @ts-ignore
-    const { accessToken } = session || {}
+    videoId: favorite.snippet.resourceId.videoId,
+    // @ts-ignore
+    title: favorite.snippet.title,
+    watchCount: 0,
+    dateAdded: '2021-10-10',
+    // @ts-ignore
+    thumbnail: favorite.snippet.thumbnails.default.url,
+  }))
+
+  return (
+    <section>
+      <Typography variant="h3" component="h2">
+        Favorites Movies list
+      </Typography>
 
 
-    const handleFetchFavorites = () => {
-        fetchFavorites(accessToken).then((fav) => {
-            console.log('fav', fav)
-            setFavorites(fav)
-        })
-    }
-
-    console.log('favorites', favorites)
-
-    useEffect(() => {
-        handleFetchFavorites()
-    }, [accessToken]);
-
-    const handleVideoDelete = async (id: string) => {
-        await deleteVideo(accessToken, id)
-        await fetchFavorites(accessToken).then((fav) => {
-            console.log('fav', fav)
-            setFavorites(fav)
-        })
-    }
-
-    const handlePlay = (id: string) => {
-        setSelectedVideoId(id)
-        setOpen(true)
-    }
-
-    console.log('favorites', favorites)
-
-    const ytVideos: Video[]  = favorites?.map((favorite) => ({
-        // @ts-ignore
-        playlistElementId: favorite.id,
-        // @ts-ignore
-        videoId: favorite.snippet.resourceId.videoId,
-        // @ts-ignore
-        title: favorite.snippet.title,
-        watchCount: 0,
-        dateAdded: '2021-10-10',
-        // @ts-ignore
-        thumbnail: favorite.snippet.thumbnails.default.url,
-    }))
-
-    return (
-        <section>
-            <Typography variant="h3" component="h2">
-                Favorites Movies list
-            </Typography>
+      {ytVideos && <VideoTable videos={ytVideos} onPlay={handlePlay} onDelete={handleVideoDelete}/>}
 
 
-            {ytVideos && <VideoTable videos={ytVideos} onPlay={handlePlay} onDelete={handleVideoDelete} />}
-
-
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <YouTube
-                        videoId={selectedVideoId}                  // defaults -> ''
-                        // id={string}                       // defaults -> ''
-                        // className={string}                // defaults -> ''
-                        // iframeClassName={string}          // defaults -> ''
-                        // style={object}                    // defaults -> {}
-                        // title={string}                    // defaults -> ''
-                        // loading={string}                  // defaults -> undefined
-                        // opts={obj}                        // defaults -> {}
-                        // onReady={func}                    // defaults -> noop
-                        // onPlay={func}                     // defaults -> noop
-                        // onPause={func}                    // defaults -> noop
-                        // onEnd={func}                      // defaults -> noop
-                        // onError={func}                    // defaults -> noop
-                        // onStateChange={func}              // defaults -> noop
-                        // onPlaybackRateChange={func}       // defaults -> noop
-                        // onPlaybackQualityChange={func}    // defaults -> noop
-                    />
-                </Box>
-            </Modal>
-            <Button onClick={handleFetchFavorites}>get</Button>
-        </section>
-    )
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <YouTube
+            videoId={selectedVideoId}
+          />
+        </Box>
+      </Modal>
+      <Button onClick={handleFetchFavorites}>get</Button>
+    </section>
+  )
 }
--- components/Table.tsx
+++ components/Table.tsx
@@ -11,85 +11,84 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
 ) {
-    return {name, calories, fat, carbs, protein};
+  return {name, calories, fat, carbs, protein};
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
 
 export interface Video {
-    playlistElementId: string;
-    videoId: string;
-    title: string;
-    thumbnail: string;
-    dateAdded: string;
-    watchCount: number;
+  playlistElementId: string;
+  videoId: string;
+  title: string;
+  thumbnail: string;
+  dateAdded: string;
+  watchCount: number;
 }
 
 interface VideoTableProps {
-    videos: Video[];
-    onDelete: (id: string) => void;
-    onPlay: (id: string) => void;
+  videos: Video[];
+  onDelete: (id: string) => void;
+  onPlay: (id: string) => void;
 }
-
 
 
 export const VideoTable: React.FC<VideoTableProps> = ({videos, onDelete, onPlay}) => {
 
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth: 650}} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Video</TableCell>
-                        <TableCell>Tutuł</TableCell>
-                        <TableCell align="right">Odtworzenia</TableCell>
-                        <TableCell align="right">Data Dodania</TableCell>
-                        <TableCell align="right">Akcje</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {videos?.map(({videoId, title, watchCount, thumbnail,playlistElementId}) => (
-                        <TableRow
-                            key={playlistElementId}
-                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                        >
-                            <TableCell component="th" scope="row">
-                                <img onClick={() => onPlay(videoId)} src={thumbnail} alt=""/>
-                            </TableCell>
-                            <TableCell component="th" scope="row">
-                                {title}
-                            </TableCell>
-                            <TableCell component="th" scope="row">
-                                {watchCount}
-                            </TableCell>
-                            <TableCell component="th" scope="row">
-                                Data dodania
-                            </TableCell><
-                            TableCell component="th" scope="row">
-                            <IconButton onClick={() => onPlay(videoId)} aria-label="delete">
-                                <PlayArrowIcon/>
-                            </IconButton>
-                            <IconButton onClick={() => onDelete(playlistElementId)} aria-label="delete">
-                                <DeleteIcon/>
-                            </IconButton>
-                        </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{minWidth: 650}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Video</TableCell>
+            <TableCell>Tutuł</TableCell>
+            <TableCell align="right">Odtworzenia</TableCell>
+            <TableCell align="right">Data Dodania</TableCell>
+            <TableCell align="right">Akcje</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {videos?.map(({videoId, title, watchCount, thumbnail, playlistElementId}) => (
+            <TableRow
+              key={playlistElementId}
+              sx={{'&:last-child td, &:last-child th': {border: 0}}}
+            >
+              <TableCell component="th" scope="row">
+                <img onClick={() => onPlay(videoId)} src={thumbnail} alt=""/>
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {title}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {watchCount}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                Data dodania
+              </TableCell><
+              TableCell component="th" scope="row">
+              <IconButton onClick={() => onPlay(videoId)} aria-label="delete">
+                <PlayArrowIcon/>
+              </IconButton>
+              <IconButton onClick={() => onDelete(playlistElementId)} aria-label="delete">
+                <DeleteIcon/>
+              </IconButton>
+            </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
